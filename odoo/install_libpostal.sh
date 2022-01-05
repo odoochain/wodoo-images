52a6bff66ab6064bdbd014e6853bc924c84f8cb3
marc <marc@itewimmer.de>
2022-01-05 15:11:22 +0100

    constraint to v1.1 libpostal

--- odoo/install_libpostal.sh
+++ odoo/install_libpostal.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 git clone https://github.com/openvenues/libpostal /root/libpostal
+pip3 install nose
 cd /root/libpostal
+git checkout v1.1
 ./bootstrap.sh
 ./configure
-make -j4
+make -j8
 make install
 ldconfig
-pip3 install nose
 pip3 install postal

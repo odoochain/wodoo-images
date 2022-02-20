3f3845a491f7f17905326af66ac88729a4061926
marc <marc@itewimmer.de>
2022-02-20 10:17:13 +0100

    versionable postgres

--- postgres/__after_compose.py
+++ postgres/__after_compose.py
@@ -2,6 +2,10 @@ import re
 import base64
 import click
 import yaml
+import inspect
+import os
+from pathlib import Path
+current_dir = Path(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
 def after_compose(config, settings, yml, globals):
     dirs = config.dirs
@@ -10,3 +14,7 @@ def after_compose(config, settings, yml, globals):
     file = dirs['run'] / 'pgcli_history'
     if not file.exists():
         file.write_text("")
+
+    # set postgres version
+    V = settings['POSTGRES_VERSION']
+    yml['services']['postgres']['build']['dockerfile'] = f'Dockerfile.{V}'

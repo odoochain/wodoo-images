8f09756802b986c09cae5f24153ce78b9a4ba07f
odoo <odoo>
2022-05-04 08:32:40 +0200

    .

--- robot/install_geckodriver.sh
+++ robot/install_geckodriver.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -ex
+apt install -y firefox
 if [[ "$TARGETARCH" == "amd64" ]]; then
-	wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+	wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.31.0-linux64.tar.gz
 	tar -xvzf geckodriver*
 	chmod +x geckodriver
 	mv geckodriver /usr/local/bin/
 
 elif [[ "$TARGETARCH" == "arm64" ]]; then
-	apt install -y firefox-geckodriver
+	# https://github.com/jamesmortensen/geckodriver-arm-binaries
+	wget https://github.com/jamesmortensen/geckodriver-arm-binaries/releases/download/v0.31.0/geckodriver-v0.31.0-linux-aarch64.tar.gz
+	tar -xvzf geckodriver*
+	chmod +x geckodriver
+	mv geckodriver /usr/local/bin/
 else
 	exit -1
 fi

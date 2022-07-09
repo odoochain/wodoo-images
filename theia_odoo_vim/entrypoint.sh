3acc8fd108d8816865726b0a81e59b53562cd71c
Marc Wimmer <marc@itewimmer.de>
2022-07-09 21:20:21 +0200

    [master]

--- theia_odoo_vim/entrypoint.sh
+++ theia_odoo_vim/entrypoint.sh
@@ -3,9 +3,6 @@
 # collect provided vsix files
 rsync -v /home/vsix_files --include=*.vsix /home/theia/plugins
 
-pip3 install -r /opt/odoo/requirements.txt
-
-
 node \
 	/home/theia/src-gen/backend/main.js \
 	/home/project \

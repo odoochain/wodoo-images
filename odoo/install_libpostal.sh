f9fcb5335d0dd2855ab31ae13c62cdd20b59add9
cicd <cicd>
2022-08-04 11:09:02 +0000

    postal

--- odoo/install_libpostal.sh
+++ odoo/install_libpostal.sh
@@ -4,15 +4,23 @@ if [[ "$ODOO_INSTALL_LIBPOSTAL" != "1" ]]; then
 	exit 0
 fi
 apt update || exit -1
-apt install -y python3 python3-venv git build-essential autoconf libtool curl
+apt install -y \
+python3 python3-venv git \
+build-essential autoconf \
+libtool curl automake \
+python-dev pkg-config
+
+WORKDIR=/usr/local/src/postal/code
+DATADIR=/usr/local/src/postal/data
+
 python3 -mvenv /root/postalenv
-git clone https://github.com/openvenues/libpostal /root/libpostal
-cd /root/libpostal
+git clone https://github.com/openvenues/libpostal "$WORKDIR"
+cd "$WORKDIR"
 /root/postalenv/bin/python3 -mpip install nose
 # 544d510db057678c16e70aa1b4598cd32d35242a
 git checkout master
 ./bootstrap.sh
-./configure
+./configure --datadir="$DATADIR"
 make -j4
 make install
 ldconfig

0ab5df1d6f90a86e43aed87a53d6bf8040ad286e
Marc Wimmer <marc@itewimmer.de>
2023-01-10 17:29:39 +0100

    is_ssl false

--- odoo/config/16.0/turndb2dev.sql
+++ odoo/config/16.0/turndb2dev.sql
@@ -4,7 +4,7 @@ update res_users set login = 'admin' where id =1;
 update ir_cron set active=false;
 delete from ir_config_parameter where key='webkit_path';
 
-/*if-table-exists fetchmail_server*/ update ir_mail_server set smtp_host='${TEST_MAIL_HOST}', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=${TEST_MAIL_SMTP_PORT};
+/*if-table-exists fetchmail_server*/ update ir_mail_server set is_ssl=false,smtp_host='${TEST_MAIL_HOST}', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=${TEST_MAIL_SMTP_PORT};
 /*if-table-exists fetchmail_server*/ alter table fetchmail_server add column if not exists server_type varchar;
 /*if-table-exists fetchmail_server*/ update fetchmail_server set server='${TEST_MAIL_HOST}', port='${TEST_MAIL_IMAP_PORT}', "user"='postmaster', password='postmaster', server_type='imap', is_ssl=false;
 

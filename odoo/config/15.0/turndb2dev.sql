4ad3332cbac52419a69975c377fe4dd0d2a839c0
Marc Wimmer <marc@itewimmer.de>
2023-01-10 21:24:32 +0100

    is ssl handling

--- odoo/config/15.0/turndb2dev.sql
+++ odoo/config/15.0/turndb2dev.sql
@@ -3,7 +3,8 @@ update res_users set password = '${DEFAULT_DEV_PASSWORD}';
 update ir_cron set active=false;
 delete from ir_config_parameter where key='webkit_path';
 
-/*if-table-exists fetchmail_server*/ update ir_mail_server set is_ssl=false,smtp_host='${TEST_MAIL_HOST}', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=${TEST_MAIL_SMTP_PORT};
+/*if-table-exists fetchmail_server*/ update ir_mail_server set smtp_host='${TEST_MAIL_HOST}', smtp_user=null, smtp_pass=null, smtp_encryption='none', smtp_port=${TEST_MAIL_SMTP_PORT};
+/*if-column-exists fetchmail_server.is_ssl*/ update ir_mail_server set is_ssl=false;
 /*if-table-exists fetchmail_server*/ alter table fetchmail_server add column if not exists server_type varchar;
 /*if-table-exists fetchmail_server*/ update fetchmail_server set server='${TEST_MAIL_HOST}', port='${TEST_MAIL_IMAP_PORT}', "user"='postmaster', password='postmaster', server_type='imap', is_ssl=false;
 

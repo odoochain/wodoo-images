56a76e711001cd1fb4cf180c0c834ab80b53fb91
Marc Wimmer <marc@itewimmer.de>
2023-04-08 00:17:59 +0200

    postgres startup configuration with comments

--- postgres/run.sh
+++ postgres/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
+function make_entrypoint_with_params() {
 python3 <<EOF
 print("Version 1.0")
 import os
 with open('/config') as file:
-    conf = file.read().split("\n")
+    conf = file.read().splitlines()
 conf += os.getenv('POSTGRES_CONFIG').split(",")
-conf = list(filter(lambda x: bool((x or '').strip()), conf))
+conf = list(filter(lambda x: bool((x or '').strip()) and not (x or '').strip().startswith("#"), conf))
 
 print("Applying configuration:\n" + '\n'.join(conf))
 
@@ -17,6 +18,8 @@ with open('/start.sh', 'w') as f:
     f.write('/usr/local/bin/docker-entrypoint.sh postgres ' + ' '.join(conf))
 
 EOF
+}
+make_entrypoint_with_params
 
 if [[ "$1" == "postgres" ]]; then
     exec gosu postgres bash /start.sh
